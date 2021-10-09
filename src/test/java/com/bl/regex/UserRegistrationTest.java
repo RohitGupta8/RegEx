17e6797600edf1e9e6b02e6ae04fd57b7f0d4c80
RohitGupta8 <therohitg@gmail.com>
2021-10-09 12:17:26 +0530

    ADD : Ability to perform Test of User need to enter a valid First Name

---
+++ src/test/java/com/bl/regex/UserRegistrationTest.java
@@ -0,0 +1,27 @@
+package com.bl.regex;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for validating first name
+ */
+public class UserRegistrationTest {
+    UserRegistration userRegistration;
+    @Before
+    public void before() {
+        userRegistration = new UserRegistration();
+    }
+
+    @Test
+    public void givenFirstNameWhenProperShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Rohit");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstNameWhenNotProperShouldReturnFalse() {
+        boolean result = userRegistration.firstName("rohit");
+        Assert.assertFalse(result);
+    }
+}
