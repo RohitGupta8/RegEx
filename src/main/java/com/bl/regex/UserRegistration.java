dfb5bfcd2badbff91c61b73b54943a562c499eb8
RohitGupta8 <therohitg@gmail.com>
2021-10-09 13:48:43 +0530

    ADD : User need to enter a valid password with at least one special char.

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -80,4 +80,14 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+
+    /**
+     * Created a method to validate password with exactly one special character
+     */
+    public boolean passwordRule4(String password) {
+        String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
