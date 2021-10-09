10943515dbc30c2b7ded09039b8989c06c78043c
RohitGupta8 <therohitg@gmail.com>
2021-10-09 13:58:53 +0530

    ADD : User need to enter a valid email.

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -90,4 +90,14 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+
+    /**
+     * Created a method to validate email
+     */
+    public boolean emailIdValidator(String emailId) {
+        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailId);
+        return matcher.matches();
+    }
 }
