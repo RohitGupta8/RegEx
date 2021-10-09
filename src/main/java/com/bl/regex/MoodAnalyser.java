296fee497221b229fa70b5378deb0ec3911ebf40
RohitGupta8 <therohitg@gmail.com>
2021-10-09 14:10:56 +0530

    ADD : Mood Analyser for user

---
+++ src/main/java/com/bl/regex/MoodAnalyser.java
@@ -0,0 +1,19 @@
+package com.bl.regex;
+
+/**
+ * Purpose  - Mood Analyser for user
+ * @author  - Rohit Gupta
+ * @version - 17.0
+ * @since   - 2021-10-09
+ */
+
+public class MoodAnalyser {
+
+    public static String analyseMood(String mood) {
+        if (mood.toLowerCase().contains("happy")) {
+            return "Entry Successful";
+        } else if (mood.toLowerCase().contains("sad")) {
+            return "Entry Failed";
+        } else return null;
+    }
+}
