601d2ca3a0d5fc389b55b6fbe39b85df0fae7796
RohitGupta8 <therohitg@gmail.com>
2021-10-09 12:33:36 +0530

    ADD : Ability to perform Test of User need to enter a valid Last Name

--- src/test/java/com/bl/regex/UserRegistrationTest.java
+++ src/test/java/com/bl/regex/UserRegistrationTest.java
@@ -1,4 +1,5 @@
 package com.bl.regex;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,11 +9,15 @@ import org.junit.Test;
  */
 public class UserRegistrationTest {
     UserRegistration userRegistration;
+
     @Before
     public void before() {
         userRegistration = new UserRegistration();
     }
 
+    /**
+     * Unit test for validating first name
+     */
     @Test
     public void givenFirstNameWhenProperShouldReturnTrue() {
         boolean result = userRegistration.firstName("Rohit");
@@ -22,6 +27,21 @@ public class UserRegistrationTest {
     @Test
     public void givenFirstNameWhenNotProperShouldReturnFalse() {
         boolean result = userRegistration.firstName("rohit");
+        Assert.assertFalse(result);
+    }
+
+    /**
+     * Unit test for validating last name
+     */
+    @Test
+    public void givenLastNameWhenProperShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Gupta");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastNameWhenNotProperShouldReturnFalse() {
+        boolean result = userRegistration.lastName("gupta");
         Assert.assertFalse(result);
     }
 }
