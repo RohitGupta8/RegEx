a5bc556322119dad5d72d1475fc70e1b4f998a4b
RohitGupta8 <therohitg@gmail.com>
2021-10-09 12:32:20 +0530

    ADD : User need to enter a valid Last name.

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -2,19 +2,32 @@ package com.bl.regex;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /**
- * Purpose  - As a User need to enter a valid First Name
- * @author  - Rohit Gupta
+ * @author - Rohit Gupta
  * @version - 17.0
- * @since   - 2021-10-09
+ * @since - 2021-10-09
  */
 
 public class UserRegistration {
 
+    /**
+     * Created a method to validate first name
+     */
     public boolean firstName(String firstName) {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    /**
+     * Created a method to validate last name
+     */
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
     }
 }
